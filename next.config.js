9d0691e5060bf0d69c8b28a38b2db5bf7aff35a5
Mariia Shabelnik <>
2023-02-02 13:25:49 +0100

    change image to img + added info to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
-@type {import('next').NextConfig}
+// @type {import('next').NextConfig}
 
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+
+        hostname: "localhost",
+        port: "10009",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+/*module.exports = {
   assetPrefix: 'http://localhost:10004/wp-admin/',
-}
+}*/
