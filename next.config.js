3c60b897c8b399cc6476efe65e786b1902d15911
Mariia Shabelnik <>
2023-02-16 18:11:44 +0100

    added botenv

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 module.exports = {
   images: {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "172.16.1.67",
+        hostname: process.env.HOST,
         pathname: "/**",
       },
     ],
