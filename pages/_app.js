5d64193bc09c162ed37df6750705547889180909
Mariia Shabelnik <>
2023-02-20 00:31:00 +0100

    Update app.js->deleted old css styles+added global

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import { ApolloProvider } from "@apollo/client/react"
-import { client } from "../lib/apollo"
-import '../styles/app.css'
+import { ApolloProvider } from "@apollo/client/react";
+import { client } from "../lib/apollo";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
-  )
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
