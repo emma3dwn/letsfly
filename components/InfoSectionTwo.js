b58787d42946c3bc5690e9dd8dde357e7f12c606
Mariia Shabelnik <>
2023-02-19 23:56:45 +0100

    Update InfoSectionTwo -> deleted old css styles

--- components/InfoSectionTwo.js
+++ components/InfoSectionTwo.js
@@ -1,21 +1,17 @@
 import React from "react";
-import styles from "../styles/infoSectionTwo.module.css";
 import Image from "next/image";
 
 function InfoSectionTwo({ post }) {
   return (
-    <section className={styles.infoSectionWrapper}>
-      <div className={styles.infoSection}>
+    <section>
+      <div>
         <Image
-          className={styles.imgScreenShot}
           src={post.infoSection.infoSectionImgTwo.mediaItemUrl}
           alt="Info Section Image Two"
           width={434}
           height={860}
         />
-        <h3 className={styles.infoSectionText}>
-          {post.infoSection.infoSectionTextTwo}
-        </h3>
+        <h3>{post.infoSection.infoSectionTextTwo}</h3>
       </div>
     </section>
   );
