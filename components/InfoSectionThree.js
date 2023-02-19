8d95c0680da366a1f571b2f87ca13cc2d244c6a0
Mariia Shabelnik <>
2023-02-19 23:51:02 +0100

    Update InfoSectionThree -> deleted old css styles

--- components/InfoSectionThree.js
+++ components/InfoSectionThree.js
@@ -1,30 +1,22 @@
 import React from "react";
-import styles from "../styles/infoSectionThree.module.css";
 import Image from "next/image";
 
 export default function InfoSectionThree({ post }) {
   return (
-    <section className={styles.infoSectionWrapper}>
-      <div className={styles.infoSection}>
+    <section>
+      <div>
         <Image
-          className={styles.imgScreenShot}
           src={post.infoSection.infoSectionImgThree.mediaItemUrl}
           alt="Info Section Image Three"
           width={726}
           height={860}
         />
 
-        <div className={styles.infoSectionTextContainer}>
-          <ol className={styles.textFieldContainer}>
-            <li className={styles.textFieldText}>
-              {post.infoSection.infoSectionThreeTextFieldOneParagraph}
-            </li>
-            <li className={styles.textFieldText}>
-              {post.infoSection.infoSectionThreeTextFieldTwoParagraph}
-            </li>
-            <li className={styles.textFieldText}>
-              {post.infoSection.infoSectionThreeTextFieldThreeParagraph}
-            </li>
+        <div>
+          <ol>
+            <li>{post.infoSection.infoSectionThreeTextFieldOneParagraph}</li>
+            <li>{post.infoSection.infoSectionThreeTextFieldTwoParagraph}</li>
+            <li>{post.infoSection.infoSectionThreeTextFieldThreeParagraph}</li>
           </ol>
         </div>
       </div>
