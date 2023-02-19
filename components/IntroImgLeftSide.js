0034cc63b98525c16a8c9a6b482b970a1784ffc2
Mariia Shabelnik <>
2023-02-20 00:02:23 +0100

    Update IntroImgLeftSide -> deleted old css styles

--- components/IntroImgLeftSide.js
+++ components/IntroImgLeftSide.js
@@ -1,25 +1,19 @@
 import React from "react";
-import styles from "../styles/introImgLeftSide.module.css";
 import Image from "next/image";
 
 function IntroImgLeftSide({ post }) {
   return (
-    <section className={styles.introSectionWrapper}>
-      <div className={styles.introSection}>
+    <section>
+      <div>
         <Image
-          className={styles.imgScreenShot}
           src={post.introSection.introSectionImgOne.mediaItemUrl}
           alt="Intro Section Image One"
           width={442}
           height={868}
         />
-        <div className={styles.introSectionTextContainer}>
-          <h2 className={styles.introSectionTitle}>
-            {post.introSection.introSectionHeadingOne}
-          </h2>
-          <p className={styles.introSectionParagraph}>
-            {post.introSection.introSectionTextOne}
-          </p>
+        <div>
+          <h2>{post.introSection.introSectionHeadingOne}</h2>
+          <p>{post.introSection.introSectionTextOne}</p>
         </div>
       </div>
     </section>
