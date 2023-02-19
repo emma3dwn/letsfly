aa7c3c7213b629d996c9d1d24260aa27e9cc926c
Mariia Shabelnik <>
2023-02-20 00:29:23 +0100

    Update SignMeUp -> deleted old css styles

--- components/SignMeUp.js
+++ components/SignMeUp.js
@@ -1,13 +1,10 @@
 import React from "react";
-import styles from "../styles/signMeUp.module.css";
 import Popup from "./Popup";
 
 function SignMeUp({ post }) {
   return (
-    <div className={styles.signMeUpWrapper}>
-      <h2 className={styles.signMeUpHeading}>
-        {post.heroSection.rightSideHeading}
-      </h2>
+    <div>
+      <h2>{post.heroSection.rightSideHeading}</h2>
       <Popup post={post} />
     </div>
   );
