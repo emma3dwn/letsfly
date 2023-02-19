af4a1936f8e1d8b7bc8f207c73f2e54716eb673b
Mariia Shabelnik <>
2023-02-19 23:27:00 +0100

    update Hero with tailwindcss

--- components/Hero.js
+++ components/Hero.js
@@ -1,13 +1,49 @@
 import React from "react";
-import styles from "../styles/hero.module.css";
 import SignMeUp from "./SignMeUp";
 import Image from "next/image";
 
 function Hero({ post }) {
   return (
+    <section
+      className="bg-local bg-center bg-full h-screen pt-20 "
+      style={{
+        backgroundImage: `url(${post.heroSection.heroBackground.mediaItemUrl})`,
+      }}
+    >
+      <div className="container mx-auto  text-white">
+        <div className="flex flex-col items-end gap-4 md:flex-row">
+          {/* HERO-TEXT LEFT SIDE  */}
+          <div className="bg-blue-900 p-2 w-full md:w-2/3 ">
+            <h1 className="text-6xl"> {post.heroSection.leftSideHeading} </h1>
+            <p> {post.heroSection.leftSideParagraph} </p>
+            <Image
+              src={post.heroSection.planeIllustration.mediaItemUrl}
+              alt="Plane Illustration"
+              width={966}
+              height={86}
+              quality={75}
+            />
+          </div>
+          {/* HERO-TEXT RIGHT SIDE  */}
+          <div className="bg-blue-900 p-2 w-full md:w-1/3 ">
+            <SignMeUp key={post.id} post={post}></SignMeUp>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
+
+/*
+
+
+return (
     <section className={styles.heroSection}>
       <div className={styles.heroTextWrapper}>
-        {/* HERO-TEXT LEFT SIDE */}
+
+        HERO-TEXT LEFT SIDE 
         <div className={styles.heroTextContainerLeft}>
           <h1 className={styles.leftSideHeading}>
             {" "}
@@ -31,7 +67,7 @@ function Hero({ post }) {
           <SignMeUp key={post.id} post={post}></SignMeUp>
         </div>
 
-        {/* PLANE ILLUSTRATION MOBILE */}
+         PLANE ILLUSTRATION MOBILE 
         <Image
           className={styles.planeIllustrationMobile}
           src={post.heroSection.planeIllustration.mediaItemUrl}
@@ -43,6 +79,4 @@ function Hero({ post }) {
       </div>
     </section>
   );
-}
-
-export default Hero;
+  */
