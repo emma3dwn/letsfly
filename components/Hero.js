9d58abc2723b0a1be80cdca7a67937ff4be2677f
Mariia Shabelnik <>
2023-02-23 15:47:20 +0100

    update text position

--- components/Hero.js
+++ components/Hero.js
@@ -1,32 +1,46 @@
 import React from "react";
 import SignMeUp from "./SignMeUp";
-import Image from "next/image";
 
 function Hero({ post }) {
   return (
     <section
-      className="bg-local bg-center bg-full h-screen pt-20 "
+      className="bg-local bg-cover bg-center bg-full h-screen "
       style={{
         backgroundImage: `url(${post.heroSection.heroBackground.mediaItemUrl})`,
       }}
     >
-      <div className="container mx-auto  text-white">
-        <div className="flex flex-col items-end gap-4 md:flex-row">
+      <div className="container mx-auto  text-white bg-blue-200/20 h-full ">
+        <div className="flex flex-col items-end gap-4 md:flex-row bg-slate-500/30 h-full pt-20 pb-5 md:py-20">
           {/* HERO-TEXT LEFT SIDE  */}
-          <div className="bg-blue-900 p-2 w-full md:w-2/3 ">
-            <h1 className="text-6xl"> {post.heroSection.leftSideHeading} </h1>
-            <p> {post.heroSection.leftSideParagraph} </p>
-            <Image
+          <div className=" p-2 w-full md:w-1/2 ">
+            <h1 className="text-7xl w-4/6 mb-10">
+              {" "}
+              {post.heroSection.leftSideHeading}{" "}
+            </h1>
+            <p className=" text-2xl text-normal tracking-wide leading-relaxed">
+              {" "}
+              {post.heroSection.leftSideParagraph}{" "}
+            </p>
+            <img
+              className="hidden md:block"
               src={post.heroSection.planeIllustration.mediaItemUrl}
               alt="Plane Illustration"
-              width={966}
-              height={86}
-              quality={75}
             />
           </div>
+
+          <div className="flex-grow"></div>
+
           {/* HERO-TEXT RIGHT SIDE  */}
-          <div className="bg-blue-900 p-2 w-full md:w-1/3 ">
-            <SignMeUp key={post.id} post={post}></SignMeUp>
+          <div className=" p-2 w-full text-center md:w-1/4 md:text-left  ">
+            <div>
+              <h2 className="text-5xl">{post.heroSection.rightSideHeading}</h2>
+
+              <img
+                className="md:hidden"
+                src={post.heroSection.planeIllustration.mediaItemUrl}
+                alt="Plane Illustration"
+              />
+            </div>
           </div>
         </div>
       </div>
