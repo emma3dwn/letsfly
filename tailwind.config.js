4d886b7eecd598ac2517dd37b5b8cd6eea15b4b8
Mariia Shabelnik <>
2023-02-26 22:11:12 +0100

    updated paddings

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,8 @@ module.exports = {
     container: {
       padding: {
         DEFAULT: "1rem",
-        sm: "0rem",
+        sm: "1rem",
+        md: "2rem",
         lg: "2rem",
         xl: "5rem",
         "2xl": "6rem",
