1ec95b177a4edbdc0ea5de22adff584b80167700
Mariia Shabelnik <>
2023-02-28 13:21:58 +0100

    update size + grid

--- components/Footer.js
+++ components/Footer.js
@@ -1,80 +1,70 @@
 import Link from "next/link";
 import React from "react";
 
-import Button from "./Button";
-import Popup from "./Popup";
 import Image from "next/image";
+
+import Popup from "./Popup";
 
 function Footer({ post }) {
   return (
-    <>
-      <section>
-        {/* LEFT SIDE */}
-        <div>
+    <footer>
+      <section className="bg-full bg-black ">
+        <div className="container m-auto p-8 text-white">
           {/* Logo */}
-          <Link href="/">
-            <Image
-              src={post.footer.footerLogo.mediaItemUrl}
-              alt="Logo"
-              width={355}
-              height={55}
-              quality={75}
-            />
-          </Link>
+          <div className="">
+            <Link href="/">
+              <img
+                className="w-56"
+                src={post.footer.footerLogo.mediaItemUrl}
+                alt="Logo"
+              />
+            </Link>
+          </div>
 
-          {/* First Links */}
-          <div>
+          <div className="flex flex-col items-left gap-2 md:flex-row">
+            {/* LEFT SIDE */}
+
+            {/* First Links */}
+
             <Link href="#">{post.footer.footerLinks.contactLink.title}</Link>
             <Link href="#">{post.footer.footerLinks.aboutUsLink.title}</Link>
             <Link href="#">{post.footer.footerLinks.faqLink.title}</Link>
-          </div>
 
-          {/* Second Links */}
-          <div>
-            <Link href="#">{post.footer.footerLinks.termsLink.title}</Link>
-            <Link href="#">{post.footer.footerLinks.privacyLink.title}</Link>
-          </div>
-        </div>
+            {/* Second Links */}
 
-        {/* RIGHT SIDE */}
-        <div>
-          <p>{post.footer.newsletterSignUp.newsletterSignUpText}</p>
-          <Popup post={post} />
-          <div>
-            <Image
-              src={post.footer.socialIcons.socialIconLinkedin.mediaItemUrl}
-              alt="Linked in"
-              width={100}
-              height={100}
-              quality={75}
-            />
-            <Image
-              src={post.footer.socialIcons.socialIconInstagram.mediaItemUrl}
-              alt="Instagram"
-              width={100}
-              height={100}
-            />
-            <Image
-              src={post.footer.socialIcons.socialIconFacebook.mediaItemUrl}
-              alt="Facebook"
-              width={100}
-              height={100}
-              quality={75}
-            />
+            {/* RIGHT SIDE */}
+            <div className=" p-2 w-full md:w-1/2 text-center md:text-right">
+              <p>{post.footer.newsletterSignUp.newsletterSignUpText}</p>
+
+              <div className="flex justify-center	gap-4 my-2">
+                <img
+                  className="w-6"
+                  src={post.footer.socialIcons.socialIconLinkedin.mediaItemUrl}
+                  alt="Linked in"
+                />
+                <img
+                  className="w-6"
+                  src={post.footer.socialIcons.socialIconInstagram.mediaItemUrl}
+                  alt="Instagram"
+                />
+                <img
+                  className="w-6"
+                  src={post.footer.socialIcons.socialIconFacebook.mediaItemUrl}
+                  alt="Facebook"
+                />
+              </div>
+            </div>
+            <div className="flex justify-center	gap-4 my-2">
+              <Link href="#">{post.footer.footerLinks.termsLink.title}</Link>
+              <Link href="#">{post.footer.footerLinks.privacyLink.title}</Link>
+            </div>
+            <div className="flex justify-center	gap-4 my-2">
+              <p> {post.footer.copyright} </p>
+            </div>
           </div>
         </div>
       </section>
-
-      {/* FOOTER BOTTOM/COPYRIGHT */}
-      <div>
-        {/* Second Links Mobile */}
-        <div>
-          <Link href="#">{post.footer.footerLinks.termsLink.title}</Link>
-          <Link href="#">{post.footer.footerLinks.privacyLink.title}</Link>
-        </div>
-        <p> {post.footer.copyright} </p>
-      </div>
-    </>
+    </footer>
   );
 }
 
