9fd064f392aed8eb24e42e3161a1c0c7c513738f
Mariia Shabelnik <>
2023-02-28 11:27:39 +0100

    update font size + padding

--- components/Banner.js
+++ components/Banner.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Banner(props) {
   return (
-    <section className="bg-black py-6 text-xl flex items-center justify-center   md:py-16 md:text-3xl lg:py-20 lg:text-4xl">
-      <h3 className="w-1/2  text-white text-center"> {props.text}</h3>
+    <section className="bg-black py-8 text-xl flex items-center justify-center   md:py-16 md:text-3xl lg:py-20 lg:text-4xl">
+      <h3 className="w-5/6 text-white text-center md:w-1/2"> {props.text}</h3>
     </section>
   );
 }
