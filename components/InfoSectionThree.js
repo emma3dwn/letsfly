e640d75c37c22ce8908d986a61ebbd11e5703477
Mariia Shabelnik <>
2023-03-03 09:49:35 +0100

    update text align

--- components/InfoSectionThree.js
+++ components/InfoSectionThree.js
@@ -7,7 +7,7 @@ export default function InfoSectionThree({ post }) {
         <div className="flex flex-col items-center gap-4 py-0 md:justify-between  md:py-16 md:flex-row">
           <div className=" p-2 w-full md:w-1/2 flex justify-around	">
             <img
-              className="h-[500px] md:h-[400px] lg:h-[500px]"
+              className="h-[360px] md:h-[380px] lg:h-[500px]"
               src={post.infoSection.infoSectionImgThree.mediaItemUrl}
               alt="Info Section Image Three"
             />
@@ -16,16 +16,16 @@ export default function InfoSectionThree({ post }) {
           <div className=" px-2 py-10 w-full md:w-1/3 text-left">
             <div className="grid grid-cols-5       md:grid-cols-3 gap-x-4 gap-y-16">
               <div className="text-right text-4xl font-mono">01</div>
-              <div className=" col-span-4 md:col-span-2 ">
+              <div className=" text-justify col-span-4 md:col-span-2 ">
                 {post.infoSection.infoSectionThreeTextFieldOneParagraph}
               </div>
               <div className="text-right text-4xl font-mono">02</div>
-              <div className=" col-span-4 md:col-span-2 ">
+              <div className="text-justify col-span-4 md:col-span-2 ">
                 {post.infoSection.infoSectionThreeTextFieldTwoParagraph}
               </div>
               <div className="text-right text-4xl font-mono">03</div>
 
-              <div className="col-span-4 md:col-span-2 ">
+              <div className="text-justify col-span-4 md:col-span-2 ">
                 {post.infoSection.infoSectionThreeTextFieldThreeParagraph}
               </div>
             </div>
